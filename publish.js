1033b39e744b3a734801b4197443bdb75403b7ed
91vx <haijiaoaa@gmail.com>
2025-07-28 16:28:10 +0800

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-          'npddjon.cc',
+          'pyenuzfg.top',
 ];
                                                                                                                   
 var JumpPage="https://91sos.com";
