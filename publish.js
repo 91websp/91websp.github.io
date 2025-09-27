bdd994d08c75ce0a1ffb734d249569c3128bd429
91vx <haijiaoaa@gmail.com>
2025-09-27 11:57:55 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-          'zeiutylo.xyz',
+          'ppevmqdi.cc',
 ];
                                                                                                                   
 var JumpPage="https://91sos.com";
