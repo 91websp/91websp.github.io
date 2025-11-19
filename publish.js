61e43816e9c3129adb9a7c926c9b1fa461ef38b7
91vx <haijiaoaa@gmail.com>
2025-11-19 19:24:14 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-          'daokiap.cc',	    
+          'gbczzonz.xyz',	    
 ];
                                                                                                                   
 var JumpPage="https://91sos.com";
