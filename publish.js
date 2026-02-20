43d23811cdb302fa86e3e41d6d3e7ab32247bb35
91vx <haijiaoaa@gmail.com>
2026-02-20 20:30:40 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-          'gkzvznw.xyz',	    
+          'mazetnrf.com',	    
 ];
                                                                                                                   
 var JumpPage="https://91sos.com";
