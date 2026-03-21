55cae43f5fcca4c85df7ce44bfc851aab8176b81
91vx <haijiaoaa@gmail.com>
2026-03-21 11:40:34 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-          'knctvbq.cc',	    
+          'wadachwz.cc',	    
 ];
                                                                                                                   
 var JumpPage="https://91sos.com";
