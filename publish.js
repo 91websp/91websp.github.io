d9f4003a5877a48ed0e284ed8160e33454af7296
91vx <haijiaoaa@gmail.com>
2026-06-19 19:07:26 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-          'bkwavpns.cc',	    
+          'kolvoymi.cc',	    
 ];
                                                                                                                   
 var JumpPage="https://91sos.com";
